42c199ff6de46e0f2598948a167dc0363a4a9219
Mike <126797494+miketreyu@users.noreply.github.com>
2024-09-26 19:39:58 +0200

    Add files via upload

---
+++ ejercicios/ejercicio1.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short a = 34;
+		System.out.print("El valor de mi variable es " + a);
+		
+
+	}
+
+}

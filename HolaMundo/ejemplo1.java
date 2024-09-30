6caa20782c566b09af61de219c316dd49a4e2812
Mike <126797494+miketreyu@users.noreply.github.com>
2024-09-30 18:13:40 +0200

    actualizacion

---
+++ HolaMundo/ejemplo1.java
@@ -0,0 +1,25 @@
+package HolaMundo;
+import java.util.Scanner;
+
+public class ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//AQUI VOY A IMPORTAR CON SCANNER
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce tu nombre: ");
+		String nombre;
+		nombre = sc.nextLine();
+		System.out.println("Introduce tu edad: ");
+		int edad = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Introduce tu DNI: ");
+		String dni; 
+		dni = sc.nextLine();
+		System.out.println(nombre);
+		System.out.println(edad);
+		System.out.println(dni);
+
+	}
+
+}
---
+++ HolaMundo/suma.java
@@ -0,0 +1,16 @@
+package HolaMundo;
+
+public class suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1, n2, suma;
+		n1 = 54;
+		n2 = 89;
+		suma = n1 + n2;
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println("la suma es: " + suma);
+	}
+
+}

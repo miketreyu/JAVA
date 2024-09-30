bc233a531e3eadd0c68720b4589456c6b7dd79b9
Mike <126797494+miketreyu@users.noreply.github.com>
2024-09-30 18:14:56 +0200

    actualizacion

--- ejercicios/ejercicio6.java
+++ ejercicios/ejercicio6.java
@@ -16,6 +16,14 @@ public class ejercicio6 {
 		System.out.println(resultado);
 		resultado = x+z/y;
 		System.out.println(resultado);
+		//ENUNCIADO
+		/*A2.7. Operadores
+Dadas las variables enteras x=3, y=4 y z=2 indica qué se asignará 
+a la variable resultado en cada expresión:
+resultado = x+y*z;
+resultado = (x+y)*z;
+resultado=++x%y;
+resultado = x+z/y; */
 		
 
 	}

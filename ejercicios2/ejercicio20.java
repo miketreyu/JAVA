43d7fedbb057e250e809c467e7c074dd254fc8d9
Mike <126797494+miketreyu@users.noreply.github.com>
2024-12-02 18:13:24 +0100

    UPDATE

--- ejercicios2/ejercicio20.java
+++ ejercicios2/ejercicio20.java
@@ -9,14 +9,13 @@ public class ejercicio20 {
 		// TODO Auto-generated method stub
 		
 		int [][] matriz ={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-		int i, j;
-		int[][] sumadiago = new int [matriz.length][matriz[0].length];
+		int i, j, cont=0;
+		int[][] sumadiago = new int [4][4];
 		
 		System.out.println("Indica las variables: ");
 		for (i=0; i<matriz.length;i++) {
-			for (j=0; j<matriz.length; j++) {
-				sumadiago[i][j] = matriz[i][(matriz.length-1)-j];
-			}
+			sumadiago += matriz[i][i];
+			sumadiago +=
 		}
 		
 		System.out.println(Arrays.deepToString(matriz));

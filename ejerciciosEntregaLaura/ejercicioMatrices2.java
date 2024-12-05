69c37f29debb4a381028f37383cdf1ba12f4aee2
Mike <126797494+miketreyu@users.noreply.github.com>
2024-12-05 14:37:14 +0100

    update

--- ejerciciosEntregaLaura/ejercicioMatrices2.java
+++ ejerciciosEntregaLaura/ejercicioMatrices2.java
@@ -1,10 +1,22 @@
-package ejerciciosEntregaLaura;
+package ejerciciosLaura1;
+import java.util.*;
 
 public class ejercicioMatrices2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Necesitamos un programa que guarde un string array en una matriz dividiendolo por espacios
+		
+		System.out.println("Digame una frase: ");
+		Scanner sc = new Scanner(System.in); // Aquí pedimos una frase
+		String frase = sc.nextLine(); // Aquí guardamos la frase en un string
+		String[] palabra = frase.split(" "); // Aquí separamos la frase por espacios
+		String [][] matriz = new String[palabra.length][]; // Aquí asignamos la cantidad de filas a la longitud de la frase dentro de la matriz
+		
+		for(int i = 0; i < palabra.length; i++) {
+			matriz[i]=palabra[i].split("");
+		}
+		System.out.println(Arrays.deepToString(matriz));
 	}
 
 }

3c252064c42987c727146f888fa568029a1828b0
Mike <126797494+miketreyu@users.noreply.github.com>
2025-01-16 21:09:36 +0100

    Add files via upload

---
+++ clases/vehiculo.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class vehiculo {
+	String marca;
+	String modelo;
+	int numPasajeros = 4;
+	int capacidadl;
+	int consumo;
+
+}
---
+++ clases/vehiculo2.java
@@ -0,0 +1,29 @@
+package clases;
+
+import java.util.Scanner;
+
+public class vehiculo2 {
+	String marca;
+	String modelo;
+	int pasajeros;
+	int capacidad;
+	int consumo;
+	
+	public vehiculo2(String marca, String modelo, int pasajeros, int capacidad, int consumo) 
+	{ 
+		this.marca = marca; 
+		this.modelo = modelo; 
+		this.pasajeros = pasajeros; 
+		this.capacidad = capacidad; 
+		this.consumo = consumo; 
+	}
+	vehiculo2 Monovolumen = new vehiculo2("Renault", "Space", 7, 50, 14);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// EL AREA DE UN TRIANGULO
+		
+
+	}
+
+}
